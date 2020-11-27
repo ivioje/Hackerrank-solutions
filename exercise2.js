bf85a87cff8b65bd7d906ca3bf3bd0d37791b61e
ivioje <rhodaivioje@gmail.com>
2020-11-27 07:01:46 +0100

    solutions

---
+++ exercise2.js
@@ -0,0 +1,14 @@
+ /**-------------
+ * Data Types: Converting from one datatype to another
+ *--------------
+ **/
+ 
+ const val = parseInt(secondInteger, 10);
+  console.log(val + firstInteger);
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    const val1 = parseFloat(secondDecimal);
+    console.log(val1 + firstDecimal);
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString + secondString);
